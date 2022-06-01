32ae244338acaa80300873253e0fac786d7d9cc7
e542323 <matthias.bachmann2@sbb.ch>
2022-06-01 13:01:19 +0200

    externalize secrets

--- containers/wordpress/start-wordpress.sh
+++ containers/wordpress/start-wordpress.sh
@@ -1,5 +1,12 @@
 echo "*** starting wordpress ***"
 
+if [[ ! -f "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_password.txt" ]] ; then
+    echo "wordpress" > "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_password.txt"
+fi
+if [[ ! -f "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_root_password.txt" ]] ; then
+    echo "wordpress" > "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_password.txt"
+fi
+
 if [ ! "$(docker ps -q -f name=wordpress)" ]; then
    docker-compose up -d
 fi
