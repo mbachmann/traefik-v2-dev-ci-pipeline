3f0262bbda7a95e48c96f670ccbbe5a84ca0db9a
e542323 <matthias.bachmann2@sbb.ch>
2022-06-01 16:38:10 +0200

    adjust wordpress

--- containers/wordpress/start-wordpress.sh
+++ containers/wordpress/start-wordpress.sh
@@ -1,10 +1,15 @@
 echo "*** starting wordpress ***"
 
+if [ -d "${CONTAINER_PERSISTENT_VOLUME}/secrets" ]; then
+  mkdir "${CONTAINER_PERSISTENT_VOLUME}/secrets"
+  echo "Creating secrects in ${CONTAINER_PERSISTENT_VOLUME}/secrets"
+fi
+
 if [[ ! -f "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_password.txt" ]] ; then
     echo "wordpress" > "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_password.txt"
 fi
 if [[ ! -f "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_root_password.txt" ]] ; then
-    echo "wordpress" > "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_password.txt"
+    echo "wordpress" > "${CONTAINER_PERSISTENT_VOLUME}/secrets/db_root_password.txt"
 fi
 
 if [ ! "$(docker ps -q -f name=wordpress)" ]; then
