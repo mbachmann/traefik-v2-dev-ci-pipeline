95be1ee4a89bc17ecf5e7bb2ca4365fed8a1ca87
bacn <mbachmann@trdi.io>
2022-07-04 06:37:16 +0200

    start webmin with redirect 80

--- traefik/start-webmin.sh
+++ traefik/start-webmin.sh
@@ -10,12 +10,14 @@ if ! grep -Fxq "redirect_host=${WEBMIN_URL}" /etc/webmin/miniserv.conf
 then
  echo "*** add  redirect_host=${WEBMIN_URL} to file /etc/webmin/miniserv.conf ***"
  echo "redirect_host=${WEBMIN_URL}" | sudo tee -a /etc/webmin/miniserv.conf
- echo "*** add  redirect_port=443 to file /etc/webmin/miniserv.conf ***"
- echo "redirect_port=443" | sudo tee -a /etc/webmin/miniserv.conf
+ echo "*** add  redirect_port=80 to file /etc/webmin/miniserv.conf ***"
+ echo "redirect_port=80" | sudo tee -a /etc/webmin/miniserv.conf
  echo "*** change ssl=1 to ssl=0 in file /etc/webmin/miniserv.conf ***"
  sudo sed -i "s/ssl=1/ssl=0/" /etc/webmin/miniserv.conf
  echo "*** restart webmin ***"
  sudo /etc/webmin/restart
+ # Open firewall to webmin service on localhost for containers
+ sudo ufw  allow  from 172.16.0.0/12 to any port 10000
 fi
 
 # echo "*** starting webmin redirect container ***"
