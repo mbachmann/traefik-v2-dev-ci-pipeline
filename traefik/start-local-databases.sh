5de68b083a211e9862d37b4dde269fe0dcfde943
bacn <mbachmann@trdi.io>
2022-08-10 13:37:13 +0700

    change to allow database accept connections from containers

--- traefik/start-local-databases.sh
+++ traefik/start-local-databases.sh
@@ -17,11 +17,14 @@ if [[ ! -f "$isMysqlInit" ]]; then
     mysql_root_password=ubuntu
     sudo systemctl stop mysql
     sudo systemctl status mysql
-    echo "set mysql binding to 0.0.0.0 and set root password"
+    echo "set mysql binding to 0.0.0.0, mysql users to % and set root password"
     sudo sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
     sudo systemctl start mysql
     sudo systemctl status mysql
     mysqladmin --user=root --password=pwd password "${mysql_root_password}"
+    # SELECT host, user FROM mysql.user;
+    # update mysql.user set host='%' where host='localhost';
+    mysql -u root -p'"'${mysql_root_password}'"' -e "update mysql.user set host='%' where host='localhost';"
     echo "set firewall rules for mysql"
     sudo ufw allow from 172.16.0.0/12 to any port 3306
     sudo ufw allow from 10.0.0.0/24 to any port 3306
